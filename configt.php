aeb6e07251359f6e75e045764d7926268bf81a58
rumor1337 <rumorpersonal@gmail.com>
2026-04-14 14:33:43 +0300

    feat: mamp or somethethetgasda

---
+++ configt.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "database" => [
+        "host" => "localhost",
+        "port" => 8889,
+        "user" => "root",
+        "password" => "root",
+        "dbname" => "quiz",
+        "charset" => "utf8mb4"
+    ]
+];
